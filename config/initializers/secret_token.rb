4409ff9cd0224efc091e28ecdab1aca7858ef006
Oskar Niburski <oskarniburski@gmail.com>
2014-01-01 01:39:55 -0500

    Created app and finished it. Should have commited earlier

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kacperpage::Application.config.secret_key_base = 'b79fe692c6b03de3cab964a63802e2ffd64519e4033d12baece6614c5d8732eaf7aca109dd784406050b2c1c72f876ccc9934d0be85d7ca92f6c54b7eb534f50'
